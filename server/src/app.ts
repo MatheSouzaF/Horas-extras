392600a14ff101b516ec01ba99fe4a3e33d3a9d2
matheussf <matheus@dzigual.com.br>
2026-02-16 10:31:28 -0300

    Ajustes

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,10 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (_request, response) => {
+  return response.status(200).json({ online: true, app: "wmeter" });
+});
+
 app.get("/health", (_request, response) => {
   return response.status(200).json({ status: "ok" });
 });
